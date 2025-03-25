81d0d717422b58e3065122e9d63c491a80706355
Waland2 <cool.drakon@gmail.com>
2025-03-25 22:41:43 +0300

    fix language field in user model

--- src/auth/models.py
+++ src/auth/models.py
@@ -9,7 +9,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(40), nullable=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=True)
-    language: Mapped[str] = mapped_column(String(3), unique=True, default="eng")
+    language: Mapped[str] = mapped_column(String(3), default="eng")
     hashed_password: Mapped[str] = mapped_column(nullable=False)
 
     cards: Mapped[list["Card"]] = relationship(back_populates="user")
